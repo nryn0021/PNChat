e02f1f408f6296d4194f6a8b9d6096a90884ff40
nryn0021 <80402194+nryn0021@users.noreply.github.com>
2023-11-08 01:57:42 +0530

    basic working of chat app

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,12 +1,20 @@
 //Node Server which will handle socket io connections
-const io = require('socket.io')(8000)
+const cors = require('cors');
+const io = require("socket.io")(8000, {
+  cors: {}
+});
+const users ={};
+io.on('connection', socket=>{           // instance which listens to all diffrent connection //all for new event
+    socket.on('new-user-joined', name =>{    // handles particular connection
+        console.log(name,"Has Joined The Chat");
+        users[socket.id] = name; 
+        socket.broadcast.emit('user-joined', name);             //brodcast tro everyone except himself  
+    });
 
-const users ={}
-
-io.on('connection', socket=>{           // instance which listens to all diffrent connection
-    socket.on('user-joined',name =>{    // handles particular connection
- 
-    })
+    socket.on('send-msg',message =>{
+        socket.broadcast.emit('deliver-msg',{message: message, name: users[socket.id]})
+     });
 })
+
 
 
